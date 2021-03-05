ab9e93723384c7bff7a0f1baa6f4fd6757cb9024
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-05 22:50:36 +0530

    day 2 switch

--- src/index.js
+++ src/index.js
@@ -89,3 +89,27 @@ console.log(getGrade(65)); */
 
 console.log(vowelsAndConsonants("javascriptloop"));
  */
+
+function getLetter(s) {
+  let letter;
+  // Write your code here
+  switch (true) {
+    case "aeiou".includes(s[0]):
+      letter = "A";
+      break;
+    case "bcdfg".includes(s[0]):
+      letter = "B";
+      break;
+    case "hjklm".includes(s[0]):
+      letter = "C";
+      break;
+    case "npqrstvwxyz".includes(s[0]):
+      letter = "D";
+      break;
+    default:
+      letter = "N/A";
+      break;
+  }
+  return letter;
+}
+console.log(getLetter("bi"));
