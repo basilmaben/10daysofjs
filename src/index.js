9ca233a1cd1f2a84c37dab5f9f70288806385a49
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-12 23:02:38 +0530

    class polygon

--- src/index.js
+++ src/index.js
@@ -185,3 +185,18 @@ console.log(rec); */
 
 console.log(getCount([1, 2, 3, 4, 5, 12, 4, 5, 6]));
  */
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  perimeter() {
+    return this.sides.reduce(function add(a, b) {
+      return a + b;
+    });
+  }
+}
+
+let triangle = new Polygon([3, 4, 5]);
+
+console.log(triangle.perimeter());
