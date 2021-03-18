3875717bcc47162658f226895067ce439903bec2
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-18 23:58:06 +0530

    bitwise op

--- src/index.js
+++ src/index.js
@@ -237,3 +237,21 @@ console.log(sides([23, 4], [3, 5])) */
 }
 
 console.log(modifyArray([1, 2, 3, 4, 4])); */
+
+var getMaxLessThanK = function (n, k) {
+  let max_value = 0;
+  for (var i = 1; i < n; i++) {
+    for (var j = i + 1; j < n + 1; j++) {
+      if (Number(i & j) < k) {
+        // This line
+        if (Number(i & j) > max_value) {
+          // And this line
+          max_value = i & j;
+        }
+      }
+    }
+  }
+  return max_value;
+};
+
+console.log(getMaxLessThanK(34, 88));
