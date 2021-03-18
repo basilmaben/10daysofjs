4c5560f1b5009e9c8878a8d384af1733cffe9f8e
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-18 23:53:34 +0530

    template literals

--- src/index.js
+++ src/index.js
@@ -227,3 +227,16 @@ let rect = new Rectangle(4, 5);
 console.log(`AREA OF SQUARE IS ${square.area()}`);
 console.log(`AREA OF RECTANGLE IS ${rect.area()}`);
  */
+
+function sides(literals, ...expressions) {
+  // ES6 allows destructuring of arrays into multiple variables
+  const [a, p] = expressions;
+  // Perform this operation only once and assign to variable
+  const root = Math.sqrt(p * p - 16 * a);
+  const s1 = (p + root) / 4;
+  const s2 = (p - root) / 4;
+  // s2 will always be smaller because of the (-/+) above
+  return [s1, s2].sort();
+}
+
+console.log(sides([23, 4], [3, 5]));
