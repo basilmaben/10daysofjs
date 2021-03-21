950f8f254abd792c99f1a8cc832371a9dcd933f4
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-21 23:55:40 +0530

    datestirng

--- src/index.js
+++ src/index.js
@@ -255,3 +255,16 @@ console.log(modifyArray([1, 2, 3, 4, 4])); */
 };
 
 console.log(getMaxLessThanK(34, 88)); */
+
+
+function getDayName(dateString) {
+  const date = new Date(dateString);
+
+  const options = {
+    weekday: 'long'
+  };
+
+  return new Intl.DateTimeFormat('en-Us', options).format(date);
+}
+
+console.log(getDayName('10/11/2009'));
