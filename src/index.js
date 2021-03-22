636e33421e640d5fb681fa53d781448f131baaeb
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-03-22 23:23:12 +0530

    regex practice

--- src/index.js
+++ src/index.js
@@ -256,14 +256,30 @@ console.log(modifyArray([1, 2, 3, 4, 4])); */
 
 console.log(getMaxLessThanK(34, 88)); */
 
-/* function getDayName(dateString) {
-  const date = new Date(dateString);
+/*  mm/dd/yyy */
 
-  const options = {
-    weekday: "long"
-  };
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+   */
+  //  ^ => first item matches:
+  // () => stores matching value captured within
+  // [aeiou] => matches any of the characters in the brackets
+  // . => matches any character:
+  // + => for 1 or more occurrances (this ensures str length > 3)
+  // \1 => matches to previously stored match.
+  // \2 looks for matched item stored 2 instances ago
+  // \3 looks for matched item stored 3 ago, etc
 
-  return new Intl.DateTimeFormat("en-Us", options).format(date);
+  //  $ ensures that matched item is at end of the sequence
+
+  let re = /^([aeiou]).+\1$/;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
 }
 
-console.log(getDayName("07/13/1998")); */
+console.log(regexVar("OOPS"));
