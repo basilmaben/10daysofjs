94c7a9ad5bd773a0c4d2f1cdf45ef4ce0077a739
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-04-03 00:00:15 +0530

    alternate char

--- src/index.js
+++ src/index.js
@@ -302,3 +302,22 @@ console.log(regexVar("OOPS"));
 }
 
 console.log(makeAnagram("b", "a", "as")); */
+
+function processData(input) {
+  var inputArray = input.split("\n");
+  var t = parseInt(inputArray[0]);
+  var pointer = 1;
+  while (pointer <= t) {
+    var target = inputArray[pointer];
+    var count = 0;
+    for (var i = 1; i < target.length; i++) {
+      if (target[i] === target[i - 1]) {
+        count += 1;
+      }
+    }
+    console.log(count);
+    pointer += 1;
+  }
+}
+
+processData("AAAA");
